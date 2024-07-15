5ab1abbea3c52d96d9d7a7d143612807574fe879
gbrods <gbrods129@gmail.com>
2024-07-15 14:45:20 -0300

    Arrumando portas do BD

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
-const PORT = process.env.PORT || 5000;
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
+const PORT = process.env.PORT;// || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
